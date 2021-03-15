926e03ee1fb3c8a2251c6bfdc1b259c3f5ff34fe
Twinkle <twinkle-rn@hotmail.com>
2021-03-15 01:12:42 +0100

    Add project files.

---
+++ ChatbutlerTest/Butler.cs
@@ -0,0 +1,17 @@
+﻿namespace ChatbutlerTest
+{
+    internal class Butler
+    {
+        public Butler()
+        {
+        }
+
+        //maak een butler die kan groeten
+        public string Greet()
+        {
+            return "Bonjour";
+        }
+
+        // maak daarna een greetings json file. file>new file> kies txt file> save as greetings.json
+    }
+}
---
+++ ChatbutlerTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChatbutlerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Butler james = new Butler();        //click ctrl+. en maak een new file Butler class
+            //laat de butler groeten
+            Console.Write(james.Greet());
+            //Console.ReadKey();
+        }
+    }
+}
